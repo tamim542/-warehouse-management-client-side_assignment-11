e69db98c84ec0e26d2a5d42c189f9cc793513e57
unknown <mdtauhidulislam509@gmail.com>
2022-05-01 23:04:41 +0600

    delete item

--- src/component/ManageInventory/ManageInventory.js
+++ src/component/ManageInventory/ManageInventory.js
@@ -9,23 +9,42 @@ const ManageInventory = () => {
 
     //----------- add item -------------
 
-    const handleForm=(event)=>{
+    const handleForm = (event) => {
         event.preventDefault();
-        const name=event.target.name.value;
-        const img=event.target.img.value;
-        const price=event.target.price.value;
-        const description=event.target.description.value;
-        const quantity=event.target.quantity.value;
-        const suppliernam=event.target.suppliernam.value;
-       const item={name,img,price,description,quantity,suppliernam};
-       fetch('http://localhost:5000/products', { method: 'POST',
-       headers: { 'Content-Type': 'application/json' },
-       body: JSON.stringify(item)
-    })
-    
-       .then(response => response.json())
-       .then(data => console.log(data));
+        const name = event.target.name.value;
+        const img = event.target.img.value;
+        const price = event.target.price.value;
+        const description = event.target.description.value;
+        const quantity = event.target.quantity.value;
+        const suppliernam = event.target.suppliernam.value;
+        const item = { name, img, price, description, quantity, suppliernam };
+        fetch('http://localhost:5000/products', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(item)
+        })
 
+            .then(response => response.json())
+            .then(data => console.log(data));
+
+    }
+
+    //------------Delete item----------------
+
+    const handleDelete = id =>{
+        const deleteItem = window.confirm('Are you confirm to delete this item?');
+        if(deleteItem){
+            const url = `http://localhost:5000/products/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const remaining = products.filter(product => product._id !== id);
+                setProducts(remaining);
+            })
+        }
     }
 
     return (
@@ -45,57 +64,65 @@ const ManageInventory = () => {
                                 </Card.Text>
                                 <Card.Title>Quantity: {product.quantity}</Card.Title>
                                 <Card.Title>Supplier Man: {product.supplier}</Card.Title>
-                                <Link to='/inventory'> <Button variant="danger">Delete</Button></Link>
+
+
+                               <Button variant="danger" onClick={() => handleDelete(product._id)}>Delete</Button>
+
+                               
                             </Card.Body>
                         </Card>
                     </div>)
                 }
 
             </div>
-             {/* --------Add new item -------- */}
+            {/* --------Add new item -------- */}
 
-             <div className='form-center'>
-            <div className='input-form'>
-            <form className='form-control' onSubmit={handleForm}>
-                
-                <h1 style={{color:'#31c75e'}}>Add New Item</h1>
-                <span> Brand Name</span>
-                <br/>
-                <input  type="text" name='name' className='input-feild' required/>
-                <br/>
-                
-                <span>Image</span>
-                <br/>
-                
-                <input  type="text" name='img' className='input-feild' required/>
-                <br/>
-                <br/>
-                <span>Price</span>
-                <br/>
-                <input type="text" name='price' className='input-feild' required/>
-                <br/>
-                <br/>
-                <span>Description</span>
-                <br/>
-                <input type="text" name='description' className='input-feild' required/>
-                <br/>
-                <br/>
-                <span>Quantity</span>
-                <br/>
-                <input type="number" name='quantity' className='input-feild' required/>
-                <br/>
-                <br/>
-                <span>Supplier Name</span>
-                <br/>
-                <input type="text" name='suppliernam' className='input-feild' required/>
-                <br/>
-                <br/>
-                <button className='button-form'><span style={{color:'white'}}>Add Item</span></button>
-                <br/>
-            </form>
-            
+            <div className='form-center'>
+                <div className='input-form'>
+                    <form className='form-control' onSubmit={handleForm}>
+
+                        <h1 style={{ color: '#31c75e' }}>Add New Item</h1>
+                        <span> Brand Name</span>
+                        <br />
+                        <input type="text" name='name' className='input-feild' required />
+                        <br />
+
+                        <span>Image</span>
+                        <br />
+
+                        <input type="text" name='img' className='input-feild' required />
+                        <br />
+                        <br />
+                        <span>Price</span>
+                        <br />
+                        <input type="text" name='price' className='input-feild' required />
+                        <br />
+                        <br />
+                        <span>Description</span>
+                        <br />
+                        <input type="text" name='description' className='input-feild' required />
+                        <br />
+                        <br />
+                        <span>Quantity</span>
+                        <br />
+                        <input type="number" name='quantity' className='input-feild' required />
+                        <br />
+                        <br />
+                        <span>Supplier Name</span>
+                        <br />
+                        <input type="text" name='suppliernam' className='input-feild' required />
+                        <br />
+                        <br />
+                        <button className='button-form'><span style={{ color: 'white' }}>Add Item</span></button>
+                        <br />
+                    </form>
+
+                </div>
             </div>
-            </div>
+
+
+            {/*---------- delete item--------*/}
+
 
 
         </div>
